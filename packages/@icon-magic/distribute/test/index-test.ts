ed7fbf007720e5641f415632ee3519ff151c075c
Ramitha Chitloor <rchitloor@linkedin.com>
2019-05-20 13:39:38 -0700

    Adding the distribute package

---
+++ packages/@icon-magic/distribute/test/index-test.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert';
+
+describe('Test test', function() {
+  it('Moves all .png files to the output directory', async () => {
+    assert.ok(true, 'Accesses exports');
+  });
+});
