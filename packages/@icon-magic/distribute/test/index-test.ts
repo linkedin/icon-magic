a323f2761b2420ead48ce5d6eae56f788517826d
Omayeli Arenyeka <aomayeli@gmail.com>
2019-06-03 14:22:20 -0700

    Revert "Updating tests"
    This reverts commit b1b245649d19cd335e0339740e5d5d3cb05732c0.

--- packages/@icon-magic/distribute/test/index-test.ts
+++ packages/@icon-magic/distribute/test/index-test.ts
@@ -12,77 +12,9 @@ const output = path.resolve(FIXTURES, 'out');
 const iconSet = configReader.getIconConfigSet(new Array(input));
 
 describe('distribute works as expected', function() {
-  it('Moves all .webp files to the right output directory', async () => {
-    await distributeByType(iconSet, output, 'webp', false);
-    const iconPath = `${output}/drawable-xxxhdpi`;
-    try {
-      if (fs.existsSync(iconPath)) {
-        assert.ok(`${iconPath} dir was generated`);
-      }
-      else {
-        assert.ok(false, `${iconPath} dir was not generated` );
-      }
-    }
-    catch(err) {
-      assert.ok(false, `${err} reading ${iconPath}`);
-    }
-  });
-
-  it('.webp files are in the right output directory', async () => {
-    await distributeByType(iconSet, output, 'webp', false);
-    const iconPath = `${output}/drawable-xxxhdpi`;
-    const icons = [
-      {
-        iconName: 'filled-1_filled-24x12@2',
-       },
-       {
-        iconName: 'filled-2_filled-24x12@2'
-      },
-      {
-        iconName: 'filled-3_filled-24x12@2'
-      },
-      {
-        iconName: 'filled-1_filled-60x60@2'
-      },
-    ];
-    const files = fs.readdirSync(iconPath);
-    icons.forEach(icon => {
-      assert.ok(files.includes(`${icon.iconName}.webp`));
-    });
-  });
-
   it('Moves all .png files to the output directory', async () => {
-    const icons = [
-      {
-        iconName: 'filled-1_filled-24x12',
-       },
-       {
-        iconName: 'filled-2_filled-24x12'
-      },
-      {
-        iconName: 'filled-3_filled-24x12'
-      },
-      {
-        iconName: 'filled-1_filled-60x60'
-      },
-    ];
     await distributeByType(iconSet, output, 'png', false);
-    icons.forEach(icon => {
-      try {
-        const iconPath = `${output}/${icon.iconName}.imageset`;
-        if (fs.existsSync(iconPath)) {
-          assert.ok(`${iconPath} dir was generated`);
-          const files = fs.readdirSync(iconPath);
-          assert.ok(files.indexOf('Contents.json') > -1, 'Contents.json was generated');
-          assert.ok(files.indexOf(`${icon.iconName}@2.png`) > -1, `${icon.iconName}@2.png was created`);
-        }
-        else {
-          assert.ok(false, `Missing files for ${iconPath}`);
-        }
-      } catch(err) {
-        assert.ok(false, err);
-      }
-    });
+    assert.ok(true, 'Accesses exports');
   });
 
   it('creates the sprite files', async () => {
@@ -104,10 +36,10 @@ describe('distribute works as expected', function() {
           assert.ok(`${p} was generated`);
         }
         else {
-          assert.ok(false, `${p} was not generated`);
+          assert.ok(false);
         }
       } catch(err) {
-        assert.ok(false, `${p} was not generated: ${err}`);
+        assert.ok(false);
       }
     });
   });
@@ -135,11 +67,10 @@ describe('distribute works as expected', function() {
       try {
         const content = fs.readFileSync(`${output}/${p.path}`, 'utf8');
         const doc = new DOMParser().parseFromString(content, 'svg');
-        const defs = doc.getElementById(p.category);
-        assert.ok(defs && defs.tagName === 'defs', 'has <defs> element with the right ID');
+        assert.ok(doc.getElementById(p.category));
         assert.ok(doc.getElementById(p.id), 'has SVG with right ID');
       } catch(err) {
-        assert.ok(false, `${err} with ${p.category}`);
+        assert.ok(false, p.category);
       }
     });
   });
