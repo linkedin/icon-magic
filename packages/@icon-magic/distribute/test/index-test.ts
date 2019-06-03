1c265028086e523f822fc81e74d100e8269ffece
Omayeli Arenyeka <aomayeli@gmail.com>
2019-06-03 14:22:20 -0700

    Revert "Updating test"
    This reverts commit b478f742b0f11e89854f6fd076831752b2a3228f.

--- packages/@icon-magic/distribute/test/index-test.ts
+++ packages/@icon-magic/distribute/test/index-test.ts
@@ -1,8 +1,6 @@
 import * as configReader from '@icon-magic/config-reader';
 import * as assert from 'assert';
-import * as fs from 'fs-extra';
 import * as path from 'path';
-import { DOMParser} from 'xmldom';
 
 import { distributeByType } from '../src';
 
@@ -17,61 +15,8 @@ describe('Test test', function() {
     assert.ok(true, 'Accesses exports');
   });
 
-  it('creates the sprite files', async () => {
+  it('creates the sprite', async () => {
     await distributeByType(iconSet, output, 'svg', true);
-    const spritePaths = [
-      {
-        path: 'icons-1.svg',
-       },
-       {
-         path: 'icons-2.svg'
-      },
-      {
-        path: 'icons-3.svg'
-      }
-    ];
-    spritePaths.forEach(p => {
-      try {
-        if (fs.existsSync(`${output}/${p.path}`)) {
-          assert.ok(`${p} was generated`);
-        }
-        else {
-          assert.ok(false);
-        }
-      } catch(err) {
-        assert.ok(false);
-      }
-    });
-  });
-
-  it('sprite files contain defs with category for ID', async () => {
-    await distributeByType(iconSet, output, 'svg', true);
-    const spritePaths = [
-      {
-        path: 'icons-1.svg',
-        id: 'home-filled-1',
-        category: 'ui-icon'
-       },
-       {
-         path: 'icons-2.svg',
-         id: 'home-filled-2',
-         category: 'uix-icon'
-      },
-      {
-        path: 'icons-3.svg',
-        id: 'home-filled-3',
-        category: 'uixx-icon'
-      }
-    ];
-    spritePaths.forEach(p => {
-      try {
-        const content = fs.readFileSync(`${output}/${p.path}`, 'utf8');
-        const doc = new DOMParser().parseFromString(content, 'svg');
-        assert.ok(doc.getElementById(p.category));
-        assert.ok(doc.getElementById(p.id), 'has SVG with right ID');
-      } catch(err) {
-        assert.ok(false, p.category);
-      }
-    });
+    assert.ok(true, 'Accesses exports');
   });
 });
