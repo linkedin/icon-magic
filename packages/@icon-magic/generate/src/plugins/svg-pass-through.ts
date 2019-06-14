608ae5963a6b592fbf2ed27df7e943cba954cdb8
ramitha <rchitloor@linkedin.com>
2019-06-14 16:32:50 -0700

    Pass through (#54)
    * Adding a pass-through for the svg type
    
    * Fixing some comments

---
+++ packages/@icon-magic/generate/src/plugins/svg-pass-through.ts
@@ -0,0 +1,36 @@
+import { Asset, Flavor, GeneratePlugin, Icon } from '@icon-magic/icon-models';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+import { SvgGenerateOptions } from './svg-generate';
+
+/**
+ * Plugin that does not change the file but copies to the output directory
+ */
+export const svgPassThrough: GeneratePlugin = {
+  name: 'svg-generate',
+  fn: async (
+    flavor: Flavor,
+    icon: Icon,
+    _params: SvgGenerateOptions = {}
+  ): Promise<Flavor> => {
+    const outputPath = icon.getIconOutputPath();
+
+    // copy the file from the input to the output path
+    await fs.mkdirp(outputPath);
+    fs.copyFileSync(
+      flavor.getPath(),
+      `${path.join(outputPath, flavor.name)}.svg`
+    );
+
+    // Create a new svg asset type and add it to the flavor
+    flavor.types.set(
+      'svg',
+      new Asset(icon.iconPath, {
+        name: flavor.name,
+        path: `./${flavor.name}.svg`
+      })
+    );
+    return flavor;
+  }
+};
