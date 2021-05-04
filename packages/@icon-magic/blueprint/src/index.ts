a18e0ec5943d141d534af1c1117f5f2abd18eac6
Omayeli Arenyeka <oarenyeka@linkedin.com>
2021-05-04 13:59:50 -0400

    Update

--- packages/@icon-magic/blueprint/src/index.ts
+++
@@ -1,3 +0,0 @@
-const foo = "test";
-
-export { foo };
--- packages/@icon-magic/blueprint/test/index-test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from "assert";
-
-import { foo } from "../src";
-
-describe("Test test", function () {
-  it("rust tests", async () => {
-    assert.ok(foo === "test", "Accesses exports");
-  });
-});
