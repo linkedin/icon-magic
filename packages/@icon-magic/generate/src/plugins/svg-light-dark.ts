34a04b98c862a4677932ba39be46afbf526c105d
Mario Alcantar <malcantar@linkedin.com>
2021-05-27 17:58:21 -0700

    complete the svg-light-dark plugin
    - correctly creates new parent svg and appends light and dark svg children
    - still need unit tests

--- packages/@icon-magic/generate/src/plugins/svg-light-dark.ts
+++ packages/@icon-magic/generate/src/plugins/svg-light-dark.ts
@@ -11,7 +11,9 @@ import {
 } from '@icon-magic/icon-models';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-// import Svgo from 'svgo';
+import Svgo from 'svgo';
+import { DOMImplementation, DOMParser } from 'xmldom';
+
 
 export interface SvgLightDarkOptions {
   lightToken?: string;
@@ -23,99 +25,105 @@ export const svgLightDark: GeneratePlugin = {
   fn: async (
     flavor: Flavor,
     icon: Icon,
-    // params: SvgLightDarkOptions = {},
+    params: SvgLightDarkOptions = {},
   ): Promise<Flavor> => {
-    // const flavorContent = (await flavor.getContents()) as string; // .svg asset's getContents() returns a string (the svg as a string)
-    // const flavorName: string = path.basename(flavor.name);
     // Create the output directory
     const outputPath = icon.getIconOutputPath();
     const imageSet = flavor.imageset;
-    // let testFlavor = icon.flavors.get("accent-1-on-dark");
-    // let testImageSet;
-    // let testColorScheme;
-
-    // if (testFlavor) {
-    //   testImageSet = testFlavor.imageset;
-    //   testColorScheme = testFlavor.colorScheme;
-    // }
 
 
-
-
+    // Only dark flavors have an imageSet Attribute
+    // If imageSet exists, plugin will run (on the "dark" flavor only)
+    // plugin will skip the "light" flavors because their data can be gotten
+    // with the dark flavor
     if (imageSet) {
-      // let lightIcon = icon.getConfig().flavors?.find(flv => flv["name"] === imageSet);
+      const darkFlavorContents = (await flavor.getContents()) as string; // .svg asset's getContents() returns a string (the svg as a string)
+      const lightFlavor = icon.flavors.get(imageSet);
+      let lightFlavorContents;
+      if (lightFlavor) {
+        lightFlavorContents = (await lightFlavor.getContents()) as string;
 
-      await fs.ensureDir(outputPath);
-      await fs.writeFile(
-        `${path.join(outputPath, flavor.name)}-YES-image-set.svg`,
-        flavor.name,
-        {
-          encoding: 'utf8'
-        }
-      );
-    // const lightToken = params.lightToken;
-    // const darkToken = params.darkToken;
-
-
-    // const lightSvgo = new Svgo({
-    //   plugins: [
-    //     {
-    //       addAttributesToSVGElement: {
-    //         attributes: [{display: lightToken}]
-    //       }
-    //     }
-    //   ],
-    //   js2svg: { pretty: true, indent: 2 }
-    // });
-
-    // const darkSvgo = new Svgo({
-    //   plugins: [
-    //     {
-    //       addAttributesToSVGElement: {
-    //         attributes: [{display: darkToken}]
-    //       }
-    //     }
-    //   ],
-    //   js2svg: { pretty: true, indent: 2 }
-    // });
-
-        // write the optimized svg to the output directory
-        // const lightAsset = await lightSvgo.optimize(flavorContent); //returns a string
-        // const darkAsset = await darkSvgo.optimize(flavorContent);
-
-        // await fs.ensureDir(outputPath);
-        // await fs.writeFile(
-        //   `${path.join(outputPath, flavor.name)}-${imageSet}-mixed.svg`,
-        //   lightAsset.data,
-        //   {
-        //     encoding: 'utf8'
-        //   }
-        // );
-        // await fs.writeFile(
-        //   `${path.join(outputPath, flavor.name)}-${imageSet}-mixed-dark.svg`,
-        //   "test",
-        //   {
-        //     encoding: 'utf8'
-        //   }
-        // );
-    } else {
-      await fs.ensureDir(outputPath);
-      await fs.writeFile(
-        `${path.join(outputPath, flavor.name)}-NO-image-set.svg`,
-        flavor.name,
-        {
-          encoding: 'utf8'
-        }
-      );
-    }
-    await fs.ensureDir(outputPath);
-    await fs.writeFile(
-      `${path.join(outputPath, flavor.name)}-ALL-light-dark.svg`,
-      icon.iconName,
-      {
-        encoding: 'utf8'
       }
-    );
+
+    const lightToken = params.lightToken;
+    const darkToken = params.darkToken;
+
+
+    const lightSvgo = new Svgo({
+      plugins: [
+        {
+          addAttributesToSVGElement: {
+            attributes: [{display: lightToken || "var(--svg-light-display)"}]
+          }
+        }
+      ],
+      js2svg: { pretty: true, indent: 2 }
+    });
+
+    const darkSvgo = new Svgo({
+      plugins: [
+        {
+          addAttributesToSVGElement: {
+            attributes: [{display: darkToken || "var(--svg-dark-display)"}]
+          }
+        }
+      ],
+      js2svg: { pretty: true, indent: 2 }
+    });
+
+      if (lightFlavorContents){
+        // create optimized light/dark svg assets
+        const lightAsset = await lightSvgo.optimize(lightFlavorContents); //returns a string
+        const darkAsset = await darkSvgo.optimize(darkFlavorContents);
+
+        // Create SVG PARENT
+        const DOM = new DOMImplementation();
+        const doctype = DOM.createDocumentType(
+          'svg',
+          '-//W3C//DTD SVG 1.1//EN',
+          'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'
+        );
+        const SVG_NS = 'http://www.w3.org/2000/svg';
+
+        // Create an SVG Document and set its doctype
+        const DOCUMENT = DOM.createDocument(SVG_NS, 'svg', doctype);
+
+        // Create SVG element
+        const svgEl = DOCUMENT.createElementNS(SVG_NS, 'svg');
+        svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        svgEl.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+        svgEl.setAttribute('version', '1.1');
+
+        // Add <svg> element to SVG Document
+        DOCUMENT.appendChild(svgEl);
+
+        let svgLight = lightAsset.data;
+        let svgDark = darkAsset.data;
+
+        // Parse XML from a string into a DOM Document.
+        const doc = new DOMParser();
+        const xmlLight = doc.parseFromString(svgLight as string, 'image/svg+xml');
+        const xmlDark = doc.parseFromString(svgDark as string, 'image/svg+xml');
+
+        svgEl.setAttribute('width', xmlLight.documentElement.getAttribute('width') || '');
+        svgEl.setAttribute('height', xmlLight.documentElement.getAttribute('height') || '');
+        svgEl.setAttribute('viewBox', xmlLight.documentElement.getAttribute('viewBox') || '');
+
+        // Append the root node of the DOM document to the parent element
+        await svgEl.appendChild(xmlLight.documentElement);
+        await svgEl.appendChild(xmlDark.documentElement);
+
+        // write the parent svg with light/dark children svg's to the output directory
+        await fs.ensureDir(outputPath);
+        await fs.writeFile(
+          `${path.join(outputPath, imageSet)}-mixed.svg`,
+          svgEl,
+          {
+            encoding: 'utf8'
+          }
+        );
+      }
+    }
     return flavor;
   }
 };
