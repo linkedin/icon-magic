99f5e3f1b5fcc442ea2a0b4ec43de16164010f08
Mario Alcantar <malcantar@linkedin.com>
2021-05-28 18:52:41 -0700

    modularize svg-light-dark, prettify svg output

--- packages/@icon-magic/generate/src/plugins/svg-light-dark.ts
+++ packages/@icon-magic/generate/src/plugins/svg-light-dark.ts
@@ -2,7 +2,8 @@
 - This plugin will create a master svg with two child svg's.
 - One dark and one light.
 - The children svgs will have a `display` attribute that can be used to set
-which of the two svg's will be visible. */
+which of the two svg's will be visible.
+*/
 
 import {
   Flavor,
@@ -12,8 +13,9 @@ import {
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import Svgo from 'svgo';
-import { DOMImplementation, DOMParser } from 'xmldom';
+import { DOMImplementation, DOMParser, XMLSerializer } from 'xmldom';
 
+const serializeToString = new XMLSerializer().serializeToString;
 
 export interface SvgLightDarkOptions {
   lightToken?: string;
@@ -30,100 +32,121 @@ export const svgLightDark: GeneratePlugin = {
     // Create the output directory
     const outputPath = icon.getIconOutputPath();
     const imageSet = flavor.imageset;
+    const colorScheme = flavor.colorScheme
 
-
-    // Only dark flavors have an imageSet Attribute
-    // If imageSet exists, plugin will run (on the "dark" flavor only)
-    // plugin will skip the "light" flavors because their data can be gotten
-    // with the dark flavor
-    if (imageSet) {
-      const darkFlavorContents = (await flavor.getContents()) as string; // .svg asset's getContents() returns a string (the svg as a string)
+    /*
+    - If `imageSet` exists and the `colorScheme` equals `dark, run the plugin.
+    - Plugin will skip the `light` flavors because their data can be collected through the `dark` flavor
+    */
+    if (imageSet && colorScheme === 'dark') {
+      // light flavor data
       const lightFlavor = icon.flavors.get(imageSet);
-      let lightFlavorContents;
-      if (lightFlavor) {
-        lightFlavorContents = (await lightFlavor.getContents()) as string;
 
-      }
+      // if light flavor doesn't exist, exit out of plugin
+      if (!lightFlavor) {
+        return flavor
+      } else {
+        const darkFlavorContents = (await flavor.getContents()) as string; // .svg asset's getContents() returns a string (the svg as a string)
+        const lightFlavorContents  = (await lightFlavor.getContents()) as string;
 
-    const lightToken = params.lightToken;
-    const darkToken = params.darkToken;
+        const lightToken = params.lightToken;
+        const darkToken = params.darkToken;
 
+        const lightSvgo = new Svgo({
+          plugins: [
+            {
+              addAttributesToSVGElement: {
+                attributes: [{display: lightToken || "var(--svg-light-display)"}]
+              }
+            }
+          ],
+          js2svg: { pretty: true, indent: 2 }
+        });
 
-    const lightSvgo = new Svgo({
-      plugins: [
-        {
-          addAttributesToSVGElement: {
-            attributes: [{display: lightToken || "var(--svg-light-display)"}]
+        const darkSvgo = new Svgo({
+          plugins: [
+            {
+              addAttributesToSVGElement: {
+                attributes: [{display: darkToken || "var(--svg-dark-display)"}]
+              }
+            }
+          ],
+          js2svg: { pretty: true, indent: 2 }
+        });
+
+        if (lightFlavorContents){
+          // create optimized light/dark svg assets
+          const lightAsset = await lightSvgo.optimize(lightFlavorContents); //returns a string
+          const darkAsset = await darkSvgo.optimize(darkFlavorContents);
+
+          const mixedParentSvg = await domSvgParentGenerate(lightAsset, darkAsset);
+
+          const mixedSvgo = new Svgo({
+            js2svg: { pretty: true, indent: 2 }
+          })
+
+          const mixedString = serializeToString(mixedParentSvg);
+          const mixedAsset = await mixedSvgo.optimize(mixedString); //returns a string
+
+          // write the parent svg with light/dark children svg's to the output directory
+          await fs.ensureDir(outputPath);
+          await fs.writeFile(
+            `${path.join(outputPath, imageSet)}-mixed.svg`,
+            mixedAsset.data,
+            {
+              encoding: 'utf8'
+            }
+          );
+          //TODO: I don't see this in the new out config yet, so still working on implementation
+          const flavorArr = icon.getConfig().flavors;
+          if (flavorArr) {
+            flavorArr.push({ name: `${imageSet}-mixed.svg`, path: `./${imageSet}-mixed.svg`, colorScheme: 'mixed' });
           }
         }
-      ],
-      js2svg: { pretty: true, indent: 2 }
-    });
-
-    const darkSvgo = new Svgo({
-      plugins: [
-        {
-          addAttributesToSVGElement: {
-            attributes: [{display: darkToken || "var(--svg-dark-display)"}]
-          }
-        }
-      ],
-      js2svg: { pretty: true, indent: 2 }
-    });
-
-      if (lightFlavorContents){
-        // create optimized light/dark svg assets
-        const lightAsset = await lightSvgo.optimize(lightFlavorContents); //returns a string
-        const darkAsset = await darkSvgo.optimize(darkFlavorContents);
-
-        // Create SVG PARENT
-        const DOM = new DOMImplementation();
-        const doctype = DOM.createDocumentType(
-          'svg',
-          '-//W3C//DTD SVG 1.1//EN',
-          'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'
-        );
-        const SVG_NS = 'http://www.w3.org/2000/svg';
-
-        // Create an SVG Document and set its doctype
-        const DOCUMENT = DOM.createDocument(SVG_NS, 'svg', doctype);
-
-        // Create SVG element
-        const svgEl = DOCUMENT.createElementNS(SVG_NS, 'svg');
-        svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        svgEl.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
-        svgEl.setAttribute('version', '1.1');
-
-        // Add <svg> element to SVG Document
-        DOCUMENT.appendChild(svgEl);
-
-        const svgLight = lightAsset.data;
-        const svgDark = darkAsset.data;
-
-        // Parse XML from a string into a DOM Document.
-        const doc = new DOMParser();
-        const xmlLight = doc.parseFromString(svgLight, 'image/svg+xml');
-        const xmlDark = doc.parseFromString(svgDark, 'image/svg+xml');
-
-        svgEl.setAttribute('width', xmlLight.documentElement.getAttribute('width') || '');
-        svgEl.setAttribute('height', xmlLight.documentElement.getAttribute('height') || '');
-        svgEl.setAttribute('viewBox', xmlLight.documentElement.getAttribute('viewBox') || '');
-
-        // Append the root node of the DOM document to the parent element
-        await svgEl.appendChild(xmlLight.documentElement);
-        await svgEl.appendChild(xmlDark.documentElement);
-
-        // write the parent svg with light/dark children svg's to the output directory
-        await fs.ensureDir(outputPath);
-        await fs.writeFile(
-          `${path.join(outputPath, imageSet)}-mixed.svg`,
-          svgEl,
-          {
-            encoding: 'utf8'
-          }
-        );
       }
     }
     return flavor;
   }
 };
+
+//TODO: lightAsset type would be `OptimizedSvg` from Svgo interface, but it does not accept this type, I think it is not exported.
+async function domSvgParentGenerate(lightAsset: any, darkAsset: any): Promise<SVGSVGElement> {
+  // Create SVG PARENT
+  const DOM = new DOMImplementation();
+  const doctype = DOM.createDocumentType(
+    'svg',
+    '-//W3C//DTD SVG 1.1//EN',
+    'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'
+  );
+  const SVG_NS = 'http://www.w3.org/2000/svg';
+
+  // Create an SVG Document and set its doctype
+  const DOCUMENT = DOM.createDocument(SVG_NS, 'svg', doctype);
+
+  // Create SVG element
+  const svgEl = DOCUMENT.createElementNS(SVG_NS, 'svg');
+  svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  svgEl.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+  svgEl.setAttribute('version', '1.1');
+
+  // Add <svg> element to SVG Document
+  DOCUMENT.appendChild(svgEl);
+
+  const svgLight = lightAsset.data;
+  const svgDark = darkAsset.data;
+
+  // Parse XML from a string into a DOM Document.
+  const doc = new DOMParser();
+  const xmlLight = doc.parseFromString(svgLight, 'image/svg+xml');
+  const xmlDark = doc.parseFromString(svgDark, 'image/svg+xml');
+
+  svgEl.setAttribute('width', xmlLight.documentElement.getAttribute('width') || '');
+  svgEl.setAttribute('height', xmlLight.documentElement.getAttribute('height') || '');
+  svgEl.setAttribute('viewBox', xmlLight.documentElement.getAttribute('viewBox') || '');
+
+  // Append the root node of the DOM document to the parent element
+  await svgEl.appendChild(xmlLight.documentElement);
+  await svgEl.appendChild(xmlDark.documentElement);
+
+  return svgEl;
+}
