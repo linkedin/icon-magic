89ef77b2bbcc28147df795cb689705f2b144e994
camario25 <33880128+camario25@users.noreply.github.com>
2021-06-14 11:57:22 -0700

    combine light and dark svg in generate plugin (#259)
    * combine light and dark svg in generate plugin
    
    * complete the svg-light-dark plugin
    
    - correctly creates new parent svg and appends light and dark svg children
    - still need unit tests
    
    * fix lint errors in svg-light-dark
    
    * modularize svg-light-dark, prettify svg output
    
    * fix missing semicolons
    
    * stop svgo from removing viewBox attribute
    
    * Add new mixed flavor to icon.flavors
    
    - it is then processed during svg-generate and added to resulting iconrc.json file.
    
    * add text fixtures for svgLightDark
    
    - still working on tests
    
    * add missing semicolon
    
    * fix test describe bug
    
    * add test fixtures and 2 tests for svgLightDark
    
    * clean unfinished tests
    
    * fix comments and formatting
    
    * combine light and dark svg in generate plugin
    
    * complete the svg-light-dark plugin
    
    - correctly creates new parent svg and appends light and dark svg children
    - still need unit tests
    
    * fix lint errors in svg-light-dark
    
    * modularize svg-light-dark, prettify svg output
    
    * fix missing semicolons
    
    * stop svgo from removing viewBox attribute
    
    * Add new mixed flavor to icon.flavors
    
    - it is then processed during svg-generate and added to resulting iconrc.json file.
    
    * add text fixtures for svgLightDark
    
    - still working on tests
    
    * add missing semicolon
    
    * fix test describe bug
    
    * add test fixtures and 2 tests for svgLightDark
    
    * clean unfinished tests
    
    * fix comments and formatting
    
    * add colorScheme flag to distribute svgs by colorScheme (#267)
    
    - can now distribute svg's by colorScheme
    - bumped commander to 7.2.0 to make variadic arguments work on options
    - added colorScheme and imageset to svg-generate so that distribute can access colorScheme in Asset by svg type
    
    Co-authored-by: Mario Alcantar <malcantar@linkedin.com>
    
    * finish svg-light-dark plugin and tests
    
    - update token names
    - update order of light and dark svgs
    - fix failing svg-light-dark tests
    
    * passes svg-generate assertion "svg has 2 paths"
    
    - svg active-small only has 1 path not 2
    - so changed test to "svg has 1 paths"
    - do not know why this was passing before?
    
    * undo last test change
    
    * combine light and dark svg in generate plugin
    
    * complete the svg-light-dark plugin
    
    - correctly creates new parent svg and appends light and dark svg children
    - still need unit tests
    
    * remove unnecessary conditionals/comments
    
    * add text fixtures for svgLightDark
    
    - still working on tests
    
    * add missing semicolon
    
    * add test fixtures and 2 tests for svgLightDark
    
    * fix comments and formatting
    
    * add colorScheme flag to distribute svgs by colorScheme (#267)
    
    - can now distribute svg's by colorScheme
    - bumped commander to 7.2.0 to make variadic arguments work on options
    - added colorScheme and imageset to svg-generate so that distribute can access colorScheme in Asset by svg type
    
    Co-authored-by: Mario Alcantar <malcantar@linkedin.com>
    
    * finish svg-light-dark plugin and tests
    
    - update token names
    - update order of light and dark svgs
    - fix failing svg-light-dark tests
    
    * passes svg-generate assertion "svg has 2 paths"
    
    - svg active-small only has 1 path not 2
    - so changed test to "svg has 1 paths"
    - do not know why this was passing before?
    
    * undo last test change
    
    * fixed test fixtures svg-light-dark
    
    Co-authored-by: Mario Alcantar <malcantar@linkedin.com>

---
+++ packages/@icon-magic/generate/src/plugins/svg-light-dark.ts
@@ -0,0 +1,179 @@
+/*
+ * This plugin will create a master svg with two child svg's.
+ * One dark and one light.
+ * The children svgs will have a `display` attribute that can be used to set
+ * which of the two svg's will be visible.
+ */
+
+import {
+  Flavor,
+  GeneratePlugin,
+  Icon
+} from '@icon-magic/icon-models';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import Svgo from 'svgo';
+import { DOMImplementation, DOMParser, XMLSerializer } from 'xmldom';
+
+const serializeToString = new XMLSerializer().serializeToString;
+
+export interface SvgLightDarkOptions {
+  lightToken?: string;
+  darkToken?: string;
+}
+
+export const svgLightDark: GeneratePlugin = {
+  name: 'svg-light-dark',
+  fn: async (
+    flavor: Flavor,
+    icon: Icon,
+    params: SvgLightDarkOptions = {},
+  ): Promise<Flavor> => {
+    // Get the output directory
+    const outputPath = icon.getIconOutputPath();
+    const imageSet = flavor.imageset;
+    const colorScheme = flavor.colorScheme;
+
+    /*
+    * If `imageSet` exists and the `colorScheme` equals `dark, run the plugin.
+    * Plugin will skip the `light` flavors because their data can be collected through the `dark` flavor
+    */
+    if (imageSet && colorScheme === 'dark') {
+      // light flavor data
+      const lightFlavor = icon.flavors.get(imageSet);
+
+      // if light flavor doesn't exist, exit out of plugin
+      if (!lightFlavor) {
+        return flavor;
+      }
+      const darkFlavorContents = (await flavor.getContents()) as string; // .svg asset's getContents() returns the svg as a string
+      const lightFlavorContents  = (await lightFlavor.getContents()) as string;
+
+      const { lightToken, darkToken } = params;
+
+      const lightSvgo = new Svgo({
+        plugins: [
+          {
+            addAttributesToSVGElement: {
+              attributes: [{display: lightToken ? `var(${lightToken})` : "var(--hue-web-svg-display-light)"}]
+            }
+          },
+          {
+            removeViewBox: false
+          }
+        ],
+        js2svg: { pretty: true, indent: 2 }
+      });
+
+      const darkSvgo = new Svgo({
+        plugins: [
+          {
+            addAttributesToSVGElement: {
+              attributes: [{display: darkToken ? `var(${darkToken})` : "var(--hue-web-svg-display-dark)"}]
+            }
+          },
+          {
+            removeViewBox: false
+          }
+        ],
+        js2svg: { pretty: true, indent: 2 }
+      });
+
+      if (lightFlavorContents) {
+        // create optimized light/dark svg assets
+        const lightAsset = await lightSvgo.optimize(lightFlavorContents); // .svg asset's getContents() returns a string
+        const darkAsset = await darkSvgo.optimize(darkFlavorContents);
+
+        const mixedParentSvg = await generateMixedSvgParent(lightAsset, darkAsset);
+
+        const mixedSvgo = new Svgo({
+          plugins: [
+            {
+              removeViewBox: false
+            }
+          ],
+          js2svg: { pretty: true, indent: 2 }
+        });
+
+        const mixedString = serializeToString(mixedParentSvg);
+        const mixedAsset = await mixedSvgo.optimize(mixedString);
+
+        // write the parent svg with light/dark children svg's to the output directory
+        await fs.ensureDir(outputPath);
+        await fs.writeFile(
+          path.format({dir: outputPath, name: `${imageSet}-mixed`, ext: '.svg'}),
+          mixedAsset.data,
+          {
+            encoding: 'utf8'
+          }
+        );
+
+        // Final new Mixed Flavor
+        const mixedFlavor: Flavor = new Flavor(icon.iconPath, {
+          name: `${imageSet}-mixed`,
+          path: `./${imageSet}-mixed.svg`,
+          colorScheme: 'mixed'
+        });
+
+        // Add new mixed flavor to icon.flavors. It is then added to resulting iconrc.json file.
+        icon.flavors.set(
+          `${imageSet}-mixed`,
+          mixedFlavor
+        );
+        return mixedFlavor;
+      }
+    }
+    return flavor;
+  }
+};
+
+// TODO: lightAsset type would be `OptimizedSvg` from Svgo interface, but it does not accept this type, looks like OptimizedSvg is not exported from Svgo.
+
+/**
+ * Takes two OptimizedSvg's from Svgo interface, adds them as children to a
+ * parent SVG Dom element and returns the Dom element
+ * @param lightAsset `OptimizedSvg` from Svgo interface
+ * @param darkAsset `OptimizedSvg` from Svgo interface
+ * @returns SVGSVGElement Dom elment with the two parameters as children.
+ */
+async function generateMixedSvgParent(lightAsset: any, darkAsset: any): Promise<SVGSVGElement> {
+  // Create SVG PARENT
+  const DOM = new DOMImplementation();
+  const doctype = DOM.createDocumentType(
+    'svg',
+    '-//W3C//DTD SVG 1.1//EN',
+    'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'
+  );
+  const SVG_NS = 'http://www.w3.org/2000/svg';
+
+  // Create an SVG Document and set its doctype
+  const DOCUMENT = DOM.createDocument(SVG_NS, 'svg', doctype);
+
+  // Create SVG element
+  const svgEl = DOCUMENT.createElementNS(SVG_NS, 'svg');
+  svgEl.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+
+  // Add <svg> element to SVG Document
+  DOCUMENT.appendChild(svgEl);
+
+  const svgLight = lightAsset.data;
+  const svgDark = darkAsset.data;
+
+  // Parse XML from a string into a DOM Document.
+  const doc = new DOMParser();
+  const xmlLight = doc.parseFromString(svgLight, 'image/svg+xml');
+  const xmlDark = doc.parseFromString(svgDark, 'image/svg+xml');
+
+  svgEl.setAttribute('width', xmlLight.documentElement.getAttribute('width') || '');
+  svgEl.setAttribute('height', xmlLight.documentElement.getAttribute('height') || '');
+  svgEl.setAttribute('viewBox', xmlLight.documentElement.getAttribute('viewBox') || '');
+
+  /*
+   * Append the root node of the DOM document to the parent element.
+   * Light is after Dark so that is the default CSS style in case the tokens fail for some reason
+  */
+  await svgEl.appendChild(xmlDark.documentElement);
+  await svgEl.appendChild(xmlLight.documentElement);
+
+  return svgEl;
+}
---
+++ packages/@icon-magic/generate/test/svg-light-dark-test.ts
@@ -0,0 +1,140 @@
+import { Flavor, Icon } from '@icon-magic/icon-models';
+import * as assert from 'assert';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+import { svgLightDark } from '../src/plugins/svg-light-dark';
+
+const FIXTURES = path.resolve(__dirname, '..', '..', 'test', 'fixtures');
+
+const lightFile = fs.readFileSync(
+  path.resolve(`${FIXTURES}/entity-ghosts/group/accent-1.svg`).toString(), 'utf-8'
+);
+
+const darkFile = fs.readFileSync(
+  path.resolve(`${FIXTURES}/entity-ghosts/group/accent-1-on-dark.svg`).toString(), 'utf-8'
+);
+
+const lightFlavor: Flavor = new Flavor(`${FIXTURES}/entity-ghosts/group/accent-1.svg`, {
+  name: 'accent-1',
+  contents: lightFile,
+  path: `${FIXTURES}/entity-ghosts/group/accent-1.svg`,
+  imageset: 'accent-1',
+  colorScheme: 'light'
+});
+
+const darkFlavor: Flavor = new Flavor(`${FIXTURES}/entity-ghosts/group/accent-1-on-dark.svg`, {
+  name: 'accent-1-on-dark',
+  contents: darkFile,
+  path: `${FIXTURES}/entity-ghosts/group/accent-1-on-dark.svg`,
+  imageset: 'accent-1',
+  colorScheme: 'dark'
+});
+
+const icon = new Icon({
+  iconPath: `${FIXTURES}/out/group`,
+  variants: [
+    {
+      path: `${FIXTURES}/entity-ghosts/group/accent-1.svg`,
+      name: 'accent-1',
+      imageset: 'accent-1',
+      colorScheme: 'light'
+    },
+    {
+      path: `${FIXTURES}/entity-ghosts/group/accent-1-on-dark.svg`,
+      name: 'accent-1-on-dark',
+      imageset: 'accent-1',
+      colorScheme: 'dark'
+    }
+  ],
+  sizes: [128],
+  resolutions: [1, 2],
+  outputPath: `/${FIXTURES}/out`,
+  iconName: 'group',
+  sourceConfigFile: `${FIXTURES}/entity-ghosts/group/iconrc.json`,
+  flavors: [
+    {
+      "name": "accent-1-on-dark",
+      "path": "./build/accent-1-on-dark.svg",
+      "imageset": "accent-1",
+      "colorScheme": "dark",
+      "types": {}
+    },
+    {
+      "name": "accent-1",
+      "path": "./build/accent-1.svg",
+      "imageset": 'accent-1',
+      "colorScheme": 'light',
+      "types": {}
+    }
+  ]
+});
+
+describe('svgLightDark()', function () {
+  it('creates parent svg with light and dark children svgs when `imageSet` exists and the `colorScheme` equals `dark`', async () => {
+  const outputSvgStripped: string = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128">
+      <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128" display="var(--hue-web-svg-display-dark)">
+        <path fill="#eee" d="M0 0h128v128H0z"/>
+        <path fill="#eee" d="M0 64.14h128V128H0z"/>
+        <path d="M32 64H0v32a32 32 0 0032-32zm64 0a32 32 0 0032 32V64zM64 96a32 32 0 0032-32H32a32 32 0 0032 32zM32 64H0V32a32.06 32.06 0 0132 32zm64 0a32.06 32.06 0 0132-32v32zM64 32a32.06 32.06 0 0132 32H32a32.06 32.06 0 0132-32z" fill="#eee"/>
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128" display="var(--hue-web-svg-display-light)">
+        <path fill="#e9e5de" d="M0 0h128v128H0z"/>
+        <path fill="#e9e5de" d="M0 64.14h128V128H0z"/>
+        <path d="M32 64H0v32a32 32 0 0032-32zm64 0a32 32 0 0032 32V64zM64 96a32 32 0 0032-32H32a32 32 0 0032 32zM32 64H0V32a32.06 32.06 0 0132 32zm64 0a32.06 32.06 0 0132-32v32zM64 32a32.06 32.06 0 0132 32H32a32.06 32.06 0 0132-32z" fill="#e9e5de"/>
+      </svg>
+    </svg>`.replace(/\s+/g, '');
+
+  const outputFlavor: Flavor = await svgLightDark.fn(darkFlavor, icon, {
+    lightToken: "--hue-web-svg-display-light",
+    darkToken: "--hue-web-svg-display-dark"
+    });
+
+    const outputFlavorBuffer = await outputFlavor.getContents();
+    const outputFlavorStringStripped = outputFlavorBuffer.toString('utf-8').replace(/\s+/g, '');
+
+    //result has proper width, height, viewbox, and display properties
+    assert.equal(outputFlavorStringStripped, outputSvgStripped);
+
+    //result does not equal original dark flavor
+    assert.notEqual(outputFlavorBuffer, darkFile);
+  });
+
+  it('returns original flavor with no changes when `colorScheme` dos not equal `dark`', async () => {
+    const outputFlavor: Flavor = await svgLightDark.fn(lightFlavor, icon, {
+      lightToken: "--hue-web-svg-display-light",
+      darkToken: "--hue-web-svg-display-dark"
+    });
+
+    //result equals original light flavor
+    assert.equal(await outputFlavor.getContents(), lightFile);
+  });
+
+  it('creates mixed svg with display property even if light/dark tags not passed from config', async () => {
+    const outputSvgStripped: string = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128">
+      <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128" display="var(--hue-web-svg-display-dark)">
+        <path fill="#eee" d="M0 0h128v128H0z"/>
+        <path fill="#eee" d="M0 64.14h128V128H0z"/>
+        <path d="M32 64H0v32a32 32 0 0032-32zm64 0a32 32 0 0032 32V64zM64 96a32 32 0 0032-32H32a32 32 0 0032 32zM32 64H0V32a32.06 32.06 0 0132 32zm64 0a32.06 32.06 0 0132-32v32zM64 32a32.06 32.06 0 0132 32H32a32.06 32.06 0 0132-32z" fill="#eee"/>
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128" display="var(--hue-web-svg-display-light)">
+        <path fill="#e9e5de" d="M0 0h128v128H0z"/>
+        <path fill="#e9e5de" d="M0 64.14h128V128H0z"/>
+        <path d="M32 64H0v32a32 32 0 0032-32zm64 0a32 32 0 0032 32V64zM64 96a32 32 0 0032-32H32a32 32 0 0032 32zM32 64H0V32a32.06 32.06 0 0132 32zm64 0a32.06 32.06 0 0132-32v32zM64 32a32.06 32.06 0 0132 32H32a32.06 32.06 0 0132-32z" fill="#e9e5de"/>
+      </svg>
+    </svg>`.replace(/\s+/g, '');
+
+    const outputFlavor: Flavor = await svgLightDark.fn(darkFlavor, icon, {});
+
+    const outputFlavorBuffer = await outputFlavor.getContents();
+    const outputFlavorStringStripped = outputFlavorBuffer.toString('utf-8').replace(/\s+/g, '');
+
+    //result has proper width, height, viewbox, and display properties
+    assert.equal(outputFlavorStringStripped, outputSvgStripped);
+
+    //result does not equal original dark flavor
+    assert.notEqual(outputFlavorBuffer, darkFile);
+  });
+});
