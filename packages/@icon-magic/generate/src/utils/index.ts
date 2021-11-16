cb50629ab90006dc9aa5c96930422564802dcfc7
Rajbir Singh Kohli <rsinghko@linkedin.com>
2021-11-16 13:29:20 -0800

    Prepare file contents and add utils

--- packages/@icon-magic/generate/src/utils/index.ts
+++ packages/@icon-magic/generate/src/utils/index.ts
@@ -17,3 +17,25 @@ export function stripSpacesBetweenTags(markup: string): string {
     .replace(/>[\t ]+</g, '><')
     .trim();
 }
+
+/**
+ * Convert a string from kebab-case to camelCase
+ * @param {string} s string to convert to camel case
+ * @returns {string} camelCased string
+ */
+export function kebabToCamel(s: string): string {
+  return s.replace(/(\-\w)/g, m => {
+    return m[1].toUpperCase();
+  });
+}
+
+/**
+ * Remove newline, tabs, and whitespaces between html tags
+ * @param {string} markup - HTML tags
+ * @returns {string}
+ */
+export function stripSpacesBetweenTags(markup: string): string {
+  return markup.replace(/\n/g, '')
+    .replace(/>[\t ]+</g, '><')
+    .trim();
+}
