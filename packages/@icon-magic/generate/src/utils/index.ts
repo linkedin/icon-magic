7eec1d1fe1964cf7d580ec1281d0feaa477a1db5
Rajbir Singh Kohli <rsinghko@linkedin.com>
2021-11-24 11:35:15 -0800

    cleanup after rebase

--- packages/@icon-magic/generate/src/utils/index.ts
+++ packages/@icon-magic/generate/src/utils/index.ts
@@ -17,23 +17,3 @@ export function stripSpacesBetweenTags(markup: string): string {
     .replace(/>[\t ]+</g, '><')
     .trim();
 }
-
-/**
- * Convert a string from kebab-case to camelCase
- * @param s string to convert to camel case
- */
-export function kebabToCamel(s: string): string {
-  return s.replace(/(\-\w)/g, m => {
-    return m[1].toUpperCase();
-  });
-}
-
-/**
- * Remove newline, tabs, and whitespaces between html tags
- * @param markup - HTML tags
- */
-export function stripSpacesBetweenTags(markup: string): string {
-  return markup && markup.replace(/\n/g, '')
-    .replace(/>[\t ]+</g, '><')
-    .trim();
-}
